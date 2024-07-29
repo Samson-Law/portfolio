86081aa0aaed54835ac3d50a0fb6ff5e8d093328
Samson Law <>
2024-07-29 20:58:17 +0100

    Update page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+import path from "path" 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
   output: "export",
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "src/styles/_general.scss";`,
+  },
 };
 
 export default nextConfig;
